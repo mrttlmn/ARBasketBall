fb6a3ebf752f86586fbb5fdd52e52acce249d09e
mrttlmn <82608763+mrttlmn@users.noreply.github.com>
2022-06-22 13:34:54 +0300

    as

--- Assets/Scripts/BallThrow.cs
+++ Assets/Scripts/BallThrow.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Niantic.ARDK.Utilities;
 using Niantic.ARDK.Utilities.Input.Legacy;
+using Niantic.ARDK.Networking.HLAPI.Object.Unity;
 
 public class BallThrow : MonoBehaviour
 {
@@ -10,22 +11,26 @@ public class BallThrow : MonoBehaviour
     private Camera arCamera;
     
     [SerializeField]
-    private GameObject ballPrefab;
+    private NetworkedUnityObject ballPrefab;
 
     private float defForce = 100.0f;
 
     public Vector3 forceVec;
-    // Update is called once per frame
    
     public void LaunchBall(float force)
     {
         forceVec = arCamera.transform.forward;
         forceVec.y += 0.5f;
-        var ball = Instantiate(ballPrefab,arCamera.transform.position,Quaternion.identity);
-        var ballRb = ball.GetComponent<Rigidbody>();
+        var newBall = ballPrefab;
+        Debug.Log(newBall.gameObject.name);
+        var ballRb = newBall.gameObject.GetComponent<Rigidbody>();
         force = force + defForce;
         if (force > 400) force = 400;
         if (force < defForce) force = defForce; 
-        ballRb.AddForce(forceVec * force);
+        ballRb.AddForce(forceVec * force,ForceMode.Impulse);
+        ballRb.useGravity = true;
+        var x = newBall.NetworkSpawn(arCamera.transform.position, Quaternion.identity);
+        
+        Debug.Log(x.name);
     }
 }
